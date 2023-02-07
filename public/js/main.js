1c98621be3bc04fae36ff92b2d3fcf389d600859
bombdylan <filipp.chess@gmail.com>
2023-02-07 12:28:20 +0300

    Added more parameters

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
 const illum = document.getElementById('Illuminance')
 const co2 = document.getElementById('CO2')
+const temp = document.getElementById('Temperature')
+const hum = document.getElementById('Humidity')
+const air = document.getElementById('AirQuality')
+const sound = document.getElementById('SoundLevel')
+const max_motion = document.getElementById('MaxMotion')
+const cur_motion = document.getElementById('CurrentMotion')
 
 const socket = io()
 
@@ -8,5 +14,23 @@ socket.on('message', ({ param, value }) => {
     illum.innerHTML = value
   } else if (param === 'CO2') {
     co2.innerHTML = value
+  }
+  else if (param === 'Temperature') {
+    temp.innerHTML = value
+  }
+  else if (param === 'Humidity') {
+    hum.innerHTML = value
+  }
+  else if (param === 'Air Quality (VOC)') {
+    air.innerHTML = value
+  }
+  else if (param === 'Sound Level') {
+    sound.innerHTML = value
+  }
+  else if (param === 'Max Motion') {
+    max_motion.innerHTML = value
+  }
+  else if (param === 'Current Motion') {
+    cur_motion.innerHTML = value
   }
 })
